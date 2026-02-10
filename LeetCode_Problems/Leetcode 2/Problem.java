fbefc3c55f74d335609df9586db60552aa4d06eb
Alok <alokgoyal634@gmail.com>
2026-02-10 18:45:08 +0530

    Added Some Problem's Solution On LeetCode

--- LeetCode_Problems/Leetcode 2/Problem.java
+++ LeetCode_Problems/Leetcode 2/Problem.java
@@ -9,10 +9,19 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummynode = new ListNode(-1);
-        ListNode currentnode = dummynode;
-        ListNode temp1 = l1; ListNode temp2 = l2; int carry = 0;
+    class Node{
+        int val;
+        Node next;
+
+        Node(int val){
+            this.val = val;
+            this.next = null;
+        }
+    }
+    public Node addTwoNumbers(Node l1, Node l2) {
+        Node dummynode = new Node(-1);
+        Node currentnode = dummynode;
+        Node temp1 = l1; Node temp2 = l2; int carry = 0;
         while(temp1 != null || temp2 != null){
             int sum = carry;
             if(temp1 != null){
@@ -23,7 +32,7 @@ class Solution {
             }
             carry = sum / 10;
             int rem = sum % 10;
-            ListNode newnode = new ListNode(rem);
+            Node newnode = new Node(rem);
             currentnode.next = newnode;
             currentnode = newnode;
             if(temp1 != null){
@@ -34,7 +43,7 @@ class Solution {
             }
         }
         if(carry != 0){
-            ListNode newnode = new ListNode(carry);
+            Node newnode = new Node(carry);
             currentnode.next = newnode;
             currentnode = newnode;
         }
